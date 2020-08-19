d6e3687d54d2d7f8533d371aa9971d09bb1968b5
tharindupr <tharindu.prf@gmail.com>
2020-08-19 17:35:15 +0100

    init configs changed

--- network/bin/init_dev_env3.sh
+++ network/bin/init_dev_env3.sh
@@ -4,6 +4,10 @@ export IMAGE_TAG=latest
 DIR="$( which $BASH_SOURCE)"
 DIR="$(dirname $DIR)"
 
+echo '================ Stoping previous containers ================'
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
 
 echo    '================ Starting the Docker Instances ================'
 docker-compose -f $DIR/../devenv/docker-compose-vm3.yaml up -d
