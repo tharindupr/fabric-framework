bbfe83e53b2e2a502a924be7e953aa556eca8622
tharindupr <tharindu.prf@gmail.com>
2020-08-20 17:02:25 +0100

    commit cc

--- network/bin/commit_chaincode.sh
+++ network/bin/commit_chaincode.sh
@@ -15,15 +15,15 @@ CC_NAME=$2
 
 commitChaincodeDefination() {
     setGlobalsForPeer0Org1
-    peer lifecycle chaincode commit -o 10.128.0.10:7050 --ordererTLSHostnameOverride orderer.digiblocks.com \
+    peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.digiblocks.com \
         --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
         --channelID $CHANNEL_NAME --name ${CC_NAME} \
         --collections-config $PRIVATE_DATA_CONFIG \
-        --peerAddresses 10.128.0.10:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
-        --peerAddresses 10.128.0.20:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
-        --peerAddresses 10.128.0.21:11051 --tlsRootCertFiles $PEER0_ORG3_CA \
-        --peerAddresses 10.128.0.22:13051 --tlsRootCertFiles $PEER0_ORG4_CA \
-        --peerAddresses 10.128.0.23:15051 --tlsRootCertFiles $PEER0_ORG5_CA \
+        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+        #--peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
+        #--peerAddresses localhost:11051 --tlsRootCertFiles $PEER0_ORG3_CA \
+        #--peerAddresses localhost:13051 --tlsRootCertFiles $PEER0_ORG4_CA \
+        #--peerAddresses localhost:15051 --tlsRootCertFiles $PEER0_ORG5_CA \
         --version ${VERSION} --sequence ${VERSION} --init-required
 
         echo "===================== Commit Successfull ===================== "
