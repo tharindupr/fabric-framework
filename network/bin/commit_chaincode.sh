10a75abdb59191c8c0ba7b345c8582841d11feef
tharindupr <tharindu.prf@gmail.com>
2020-08-20 17:02:25 +0100

    commit

--- network/bin/commit_chaincode.sh
+++ network/bin/commit_chaincode.sh
@@ -15,11 +15,11 @@ CC_NAME=$2
 
 commitChaincodeDefination() {
     setGlobalsForPeer0Org1
-    peer lifecycle chaincode commit -o 10.128.0.10:7050 --ordererTLSHostnameOverride orderer.digiblocks.com \
+    peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.digiblocks.com \
         --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
         --channelID $CHANNEL_NAME --name ${CC_NAME} \
         --collections-config $PRIVATE_DATA_CONFIG \
-        --peerAddresses 10.128.0.10:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
         --peerAddresses 10.128.0.20:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
         --peerAddresses 10.128.0.21:11051 --tlsRootCertFiles $PEER0_ORG3_CA \
         --peerAddresses 10.128.0.22:13051 --tlsRootCertFiles $PEER0_ORG4_CA \
