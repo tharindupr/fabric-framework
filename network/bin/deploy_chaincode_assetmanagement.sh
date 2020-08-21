9407270c25326845f35451d9ab07630bfaa65e77
tharindupr <tharindu.prf@gmail.com>
2020-08-21 22:19:46 +0100

    d

--- network/bin/deploy_chaincode_assetmanagement.sh
+++ network/bin/deploy_chaincode_assetmanagement.sh
@@ -5,6 +5,5 @@
 ./approve_chaincode.sh "./../../gocc/src/github.com/tharindupr/asset_management" "assetcontract" $1
 ./commit_chaincode.sh "./../../gocc/src/github.com/tharindupr/asset_management" "assetcontract" $1
 
-
 echo "===================== Invoking Init ====================="
 ./invoke_init.sh assetcontract $1
