2641f3ac89fe23705f8cc9777cff49a4e13f156f
tharindupr <tharindu.prf@gmail.com>
2020-08-24 17:37:57 +0000

    commit chaincode.sh

--- network/bin/commit_chaincode.sh
+++ network/bin/commit_chaincode.sh
@@ -15,15 +15,15 @@ CC_NAME=$2
 
 commitChaincodeDefination() {
     setGlobalsForPeer0Org1
-    peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.digiblocks.com \
-        --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
+    peer lifecycle chaincode commit -o orderer.digiblocks.com:7050 \
+        --tls --cafile $ORDERER_CA \
         --channelID $CHANNEL_NAME --name ${CC_NAME} \
         --collections-config $PRIVATE_DATA_CONFIG \
-        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
-	--peerAddresses 10.128.0.20:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
-        --peerAddresses 10.128.0.21:11051 --tlsRootCertFiles $PEER0_ORG3_CA \
-        --peerAddresses 10.128.0.22:13051 --tlsRootCertFiles $PEER0_ORG4_CA \
-        --peerAddresses 10.128.0.23:15051 --tlsRootCertFiles $PEER0_ORG5_CA \
+        --peerAddresses peer0.org1.digiblocks.com:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+	--peerAddresses peer0.org2.digiblocks.com:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
+        --peerAddresses peer0.org3.digiblocks.com:11051 --tlsRootCertFiles $PEER0_ORG3_CA \
+        --peerAddresses peer0.org4.digiblocks.com:13051 --tlsRootCertFiles $PEER0_ORG4_CA \
+        --peerAddresses peer0.org5.digiblocks.com:15051 --tlsRootCertFiles $PEER0_ORG5_CA \
         --connTimeout 60s \
 	--waitForEventTimeout 60s \
 	--version ${VERSION} --sequence ${VERSION} --init-required
