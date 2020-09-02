9fe3416e142672152e3156c184d8bdcd1c5b2b07
tharindupr <tharindu.prf@gmail.com>
2020-09-02 12:03:26 +0100

    createObject

--- network/bin/invoke_createObject.sh
+++ network/bin/invoke_createObject.sh
@@ -25,7 +25,7 @@ chaincodeInvoke() {
         --peerAddresses peer0.org2.digiblocks.com:9051 --tlsRootCertFiles $PEER0_ORG2_CA   \
         --peerAddresses peer0.org3.digiblocks.com:11051 --tlsRootCertFiles $PEER0_ORG3_CA   \
         --peerAddresses peer0.org5.digiblocks.com:15051 --tlsRootCertFiles $PEER0_ORG5_CA   \
-        -c '{"function": "createSubject","Args":["Object-ABCDEEE1", "Object","{\"manufacturer\":\"samsung\", \"organization\": \"org2\",\"location\":\"org1-bulding-01\"}"]}'
+        -c '{"function": "createObject","Args":["Object-ABCDEEE1", "Object","{\"manufacturer\":\"samsung\", \"organization\": \"org2\",\"location\":\"org1-bulding-01\"}"]}'
 
     ## Init ledger
     # peer chaincode invoke -o localhost:7050 \
