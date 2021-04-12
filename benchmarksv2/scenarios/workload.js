19fe5bda6a68f40acc52029e783354a4b998c843
tharindupr <tharindu.prf@gmail.com>
2021-04-13 00:10:56 +0100

    benchmark v2

---
+++ benchmarksv2/scenarios/workload.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+class MyWorkload extends WorkloadModuleBase {
+    constructor() {
+        super();
+    }
+    async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
+        await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+    }
+    
+    async submitTransaction() {
+        const randomId = Math.floor(Math.random()*this.roundArguments.assets);
+        const myArgs = {
+            contractId: this.roundArguments.contractId,
+            contractFunction: 'createAsset',
+            invokerIdentity: 'client0.org1.example.com',
+            contractArguments: [randomId, "Building","Pending", "{\"attribute1_name\":\"value1\", \"attribute2_name\":\"value2\",\"attribute3_name\":\"value3\"}"],
+            readOnly: false
+        };
+        await this.sutAdapter.sendRequests(myArgs);
+    }
+    
+    async cleanupWorkloadModule() {
+        // NOOP
+    }
+}
+function createWorkloadModule() {
+    return new MyWorkload();
+}
+module.exports.createWorkloadModule = createWorkloadModule;
