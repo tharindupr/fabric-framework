0881580029114f6dc825cf89603f13c42cc2d04f
tharindupr <tharindu.prf@gmail.com>
2021-04-20 20:35:03 +0100

    approve_chaincode.sh

---
+++ network/bin/approve_chaincode.sh
@@ -0,0 +1,114 @@
+. ./set_env.sh --source-only
+
+export PRIVATE_DATA_CONFIG=${1}/collections_config.json
+
+CHANNEL_NAME="mychannel"
+CC_RUNTIME_LANGUAGE="golang"
+VERSION=$3
+CC_SRC_PATH=$1
+CC_NAME=$2
+
+
+
+queryInstalled() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode queryinstalled >&log.txt
+    cat log.txt
+    PACKAGE_ID=$(sed -n "/${CC_NAME}_${VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
+    echo PackageID is ${PACKAGE_ID}
+    echo "===================== Query installed successful on peer0.org1 on channel ===================== "
+}
+
+# queryInstalled
+
+# --collections-config ./artifacts/private-data/collections_config.json \
+#         --signature-policy "OR('Org1MSP.member','Org2MSP.member')" \
+# --collections-config $PRIVATE_DATA_CONFIG \
+
+approveForMyOrg1() {
+    setGlobalsForPeer0Org1
+    # set -x
+    peer lifecycle chaincode approveformyorg -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.digiblocks.com --tls \
+        --collections-config $PRIVATE_DATA_CONFIG \
+        --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
+        --init-required --package-id ${PACKAGE_ID} \
+        --sequence ${VERSION}
+    # set +x
+
+    echo "===================== chaincode approved from org 1 ===================== "
+
+}
+
+approveForMyOrg2() {
+    setGlobalsForPeer0Org2
+
+    peer lifecycle chaincode approveformyorg -o $ORDERER_IP:7050 \
+        --ordererTLSHostnameOverride orderer.digiblocks.com --tls $CORE_PEER_TLS_ENABLED \
+        --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} \
+        --collections-config $PRIVATE_DATA_CONFIG \
+        --version ${VERSION} --init-required --package-id ${PACKAGE_ID} \
+        --sequence ${VERSION}
+
+    echo "===================== chaincode approved from org 2 ===================== "
+}
+
+
+
+approveForMyOrg3() {
+    setGlobalsForPeer0Org3
+
+    peer lifecycle chaincode approveformyorg -o $ORDERER_IP:7050 \
+        --ordererTLSHostnameOverride orderer.digiblocks.com --tls $CORE_PEER_TLS_ENABLED \
+        --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} \
+        --collections-config $PRIVATE_DATA_CONFIG \
+        --version ${VERSION} --init-required --package-id ${PACKAGE_ID} \
+        --sequence ${VERSION}
+
+    echo "===================== chaincode approved from org 3 ===================== "
+}
+
+
+
+approveForMyOrg4() {
+    setGlobalsForPeer0Org4
+
+    peer lifecycle chaincode approveformyorg -o $ORDERER_IP:7050 \
+        --ordererTLSHostnameOverride orderer.digiblocks.com --tls $CORE_PEER_TLS_ENABLED \
+        --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} \
+        --collections-config $PRIVATE_DATA_CONFIG \
+        --version ${VERSION} --init-required --package-id ${PACKAGE_ID} \
+        --sequence ${VERSION}
+
+    echo "===================== chaincode approved from org 4 ===================== "
+}
+
+
+
+approveForMyOrg5() {
+    setGlobalsForPeer0Org5
+
+    peer lifecycle chaincode approveformyorg -o $ORDERER_IP:7050 \
+        --ordererTLSHostnameOverride orderer.digiblocks.com --tls $CORE_PEER_TLS_ENABLED \
+        --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} \
+        --collections-config $PRIVATE_DATA_CONFIG \
+        --version ${VERSION} --init-required --package-id ${PACKAGE_ID} \
+        --sequence ${VERSION}
+
+    echo "===================== chaincode approved from org 5 ===================== "
+}
+
+checkCommitReadyness() {
+
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode checkcommitreadiness --channelID $CHANNEL_NAME \
+        --peerAddresses peer0.org1.digiblocks.com:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+        --collections-config $PRIVATE_DATA_CONFIG \
+        --name ${CC_NAME} --version ${VERSION} --sequence ${VERSION} --output json --init-required
+    echo "===================== checking commit readyness from org 1 ===================== "
+}
+
+queryInstalled
+approveForMyOrg1
+approveForMyOrg2
+checkCommitReadyness
