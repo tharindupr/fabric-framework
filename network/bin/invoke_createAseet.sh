419bab31342581e8038ecff4fa681373e557b916
tharindupr <tharindu.prf@gmail.com>
2021-04-24 15:10:57 +0100

    invoke

---
+++ network/bin/invoke_createAseet.sh
@@ -0,0 +1,60 @@
+. ./set_env.sh --source-only
+
+CHANNEL_NAME="mychannel"
+CC_RUNTIME_LANGUAGE="golang"
+VERSION="1"
+CC_NAME=identitycontract
+
+chaincodeInvoke() {
+    # setGlobalsForPeer0Org1
+    # peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com \
+    # --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n ${CC_NAME} \
+    # --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+    # --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA  \
+    # -c '{"function":"initLedger","Args":[]}'
+
+    setGlobalsForPeer0Org1
+
+    ## Create Subject
+    peer chaincode invoke -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.digiblocks.com \
+        --tls $CORE_PEER_TLS_ENABLED \
+        --cafile $ORDERER_CA \
+        -C $CHANNEL_NAME -n ${CC_NAME}  \
+        --peerAddresses peer0.org1.digiblocks.com:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+        --peerAddresses peer0.org2.digiblocks.com:9051 --tlsRootCertFiles $PEER0_ORG2_CA   \
+        --peerAddresses peer0.org3.digiblocks.com:11051 --tlsRootCertFiles $PEER0_ORG3_CA   \
+        --peerAddresses peer0.org5.digiblocks.com:15051 --tlsRootCertFiles $PEER0_ORG5_CA   \
+        -c '{"function": "createAsset","Args":["Nimbus001", "Building","Pending", "{\"attribute1_name\":\"value1\", \"attribute2_name\":\"value2\",\"attribute3_name\":\"value3\"}"]}'
+
+    ## Init ledger
+    # peer chaincode invoke -o localhost:7050 \
+    #     --ordererTLSHostnameOverride orderer.digiblocks.com \
+    #     --tls $CORE_PEER_TLS_ENABLED \
+    #     --cafile $ORDERER_CA \
+    #     -C $CHANNEL_NAME -n ${CC_NAME} \
+    #     --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+    #     --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
+    #     -c '{"function": "initLedger","Args":[]}'
+
+    ## Add private data
+    # export CAR=$(echo -n "{\"key\":\"1111\", \"make\":\"Tesla\",\"model\":\"Tesla A1\",\"color\":\"White\",\"owner\":\"tharindu\",\"price\":\"10000\"}" | base64 | tr -d \\n)
+    # peer chaincode invoke -o localhost:7050 \
+    #     --ordererTLSHostnameOverride orderer.digiblocks.com \
+    #     --tls $CORE_PEER_TLS_ENABLED \
+    #     --cafile $ORDERER_CA \
+    #     -C $CHANNEL_NAME -n ${CC_NAME} \
+    #     --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+    #     --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
+    #     -c '{"function": "createPrivateCar", "Args":[]}' \
+    #     --transient "{\"car\":\"$CAR\"}"
+}
+
+
+
+
+
+
+#chaincodeInvoke1
+#chaincodeQuery
+chaincodeInvoke
---
+++ network/bin/invoke_createDEC.sh
@@ -0,0 +1,58 @@
+. ./set_env.sh --source-only
+
+CHANNEL_NAME="mychannel"
+CC_RUNTIME_LANGUAGE="golang"
+VERSION="1"
+CC_NAME=deccontract
+
+chaincodeInvoke() {
+    # setGlobalsForPeer0Org1
+    # peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com \
+    # --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n ${CC_NAME} \
+    # --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+    # --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA  \
+    # -c '{"function":"initLedger","Args":[]}'
+
+    setGlobalsForPeer0Org1
+
+    ## Create Subject
+    peer chaincode invoke -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.digiblocks.com \
+        --tls $CORE_PEER_TLS_ENABLED \
+        --cafile $ORDERER_CA \
+        -C $CHANNEL_NAME -n ${CC_NAME}  \
+        --peerAddresses peer0.org1.digiblocks.com:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+        --peerAddresses peer0.org2.digiblocks.com:9051 --tlsRootCertFiles $PEER0_ORG2_CA   \
+        -c '{"function": "createDEC","Args":["DEC0000X", "Nimbus001", "School","123.5","8", "245.8", "02/07/2020", "02/08/2020", "C"]}'
+
+    ## Init ledger
+    # peer chaincode invoke -o localhost:7050 \
+    #     --ordererTLSHostnameOverride orderer.digiblocks.com \
+    #     --tls $CORE_PEER_TLS_ENABLED \
+    #     --cafile $ORDERER_CA \
+    #     -C $CHANNEL_NAME -n ${CC_NAME} \
+    #     --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+    #     --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
+    #     -c '{"function": "initLedger","Args":[]}'
+
+    ## Add private data
+    # export CAR=$(echo -n "{\"key\":\"1111\", \"make\":\"Tesla\",\"model\":\"Tesla A1\",\"color\":\"White\",\"owner\":\"tharindu\",\"price\":\"10000\"}" | base64 | tr -d \\n)
+    # peer chaincode invoke -o localhost:7050 \
+    #     --ordererTLSHostnameOverride orderer.digiblocks.com \
+    #     --tls $CORE_PEER_TLS_ENABLED \
+    #     --cafile $ORDERER_CA \
+    #     -C $CHANNEL_NAME -n ${CC_NAME} \
+    #     --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+    #     --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
+    #     -c '{"function": "createPrivateCar", "Args":[]}' \
+    #     --transient "{\"car\":\"$CAR\"}"
+}
+
+
+
+
+
+
+#chaincodeInvoke1
+#chaincodeQuery
+chaincodeInvoke
