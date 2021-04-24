71b5c88f64f205fdf003e241c5fa63bb59d9831e
tharindupr <tharindu.prf@gmail.com>
2021-04-24 13:39:57 +0100

    peer 2 removed

--- network/bin/invoke_init.sh
+++ network/bin/invoke_init.sh
@@ -11,8 +11,8 @@ chaincodeInvokeInit() {
         --ordererTLSHostnameOverride orderer.digiblocks.com \
         --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
         -C $CHANNEL_NAME -n ${CC_NAME} \
-        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
-        --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
+        --peerAddresses peer0.org1.digiblocks.com:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+        --peerAddresses peer0.org2.digiblocks.com:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
         --isInit -c '{"Args":[]}'
 
 }
