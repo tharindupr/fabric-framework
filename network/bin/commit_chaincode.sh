e4fafa3781277a225575ae5b7290d811797fa4b3
tharindupr <tharindu.prf@gmail.com>
2021-04-24 13:34:59 +0100

    commit_chaincode.sh

--- network/bin/commit_chaincode.sh
+++ network/bin/commit_chaincode.sh
@@ -19,8 +19,8 @@ commitChaincodeDefination() {
         --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
         --channelID $CHANNEL_NAME --name ${CC_NAME} \
         --collections-config $PRIVATE_DATA_CONFIG \
-        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
-        --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
+        --peerAddresses peer0.org1.digiblocks.com:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+        --peerAddresses peer0.org2.digiblocks.com:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
         --version ${VERSION} --sequence ${VERSION} --init-required
 
         echo "===================== Commit Successfull ===================== "
