068cdebed60b43951d1fb4bedc0aef1d61fd7f30
tharindupr <tharindu.prf@gmail.com>
2021-04-24 15:11:53 +0100

    config-createAsset.yaml

---
+++ benchmarks/scenarios/createAsset.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+class MyWorkload extends WorkloadModuleBase {
+    constructor() {
+        super();
+    }
+    async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
+        await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+    }
+    
+    async submitTransaction() {
+        const randomId1 = Math.floor(Math.random()*1000);
+        const randomId2 = Math.floor(Math.random()*20000000);
+        const myArgs = {
+            contractId: this.roundArguments.contractId,
+            contractFunction: 'createAsset',
+            invokerIdentity: 'client0.org1.digiblocks.com',
+            contractArguments: [`${this.workerIndex}_${randomId1}__${randomId2}`, "Building","Pending", "{\"attribute1_name\":\"value1\", \"attribute2_name\":\"value2\",\"attribute3_name\":\"value3\"}"],
+            readOnly: false
+        };
+        await this.sutAdapter.sendRequests(myArgs);
+    }
+    
+    async cleanupWorkloadModule() {
+        // NOOP
+    }
+}
+function createWorkloadModule() {
+    return new MyWorkload();
+}
+module.exports.createWorkloadModule = createWorkloadModule;
