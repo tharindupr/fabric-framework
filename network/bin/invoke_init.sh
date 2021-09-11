e51e044e2ff30248d698a58af1bdf02f5c51c9dc
tharindupr <tharindu.prf@gmail.com>
2021-09-11 15:03:47 +0100

    invoke init

--- network/bin/invoke_init.sh
+++ network/bin/invoke_init.sh
@@ -13,8 +13,8 @@ chaincodeInvokeInit() {
         --peerAddresses peer0.org1.digiblocks.com:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
         --peerAddresses peer0.org2.digiblocks.com:9051 --tlsRootCertFiles $PEER0_ORG2_CA \
         --peerAddresses peer0.org3.digiblocks.com:11051 --tlsRootCertFiles $PEER0_ORG3_CA \
-        --peerAddresses peer0.org4.digiblocks.com:13051 --tlsRootCertFiles $PEER0_ORG4_CA \
-	--peerAddresses peer0.org5.digiblocks.com:15051 --tlsRootCertFiles $PEER0_ORG5_CA \
+        # --peerAddresses peer0.org4.digiblocks.com:13051 --tlsRootCertFiles $PEER0_ORG4_CA \
+	# --peerAddresses peer0.org5.digiblocks.com:15051 --tlsRootCertFiles $PEER0_ORG5_CA \
 	--isInit -c '{"Args":[]}'
 
 }
