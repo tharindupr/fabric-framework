5de2958851d6b5f32e09d5b16a71e78adda29894
tharindupr <tharindu.prf@gmail.com>
2021-09-11 13:22:10 +0100

    model

--- network/bin/invoke_createModel1.sh
+++ network/bin/invoke_createModel1.sh
@@ -25,10 +25,7 @@ chaincodeInvoke() {
         --peerAddresses peer0.org2.digiblocks.com:9051 --tlsRootCertFiles $PEER0_ORG2_CA   \
         --peerAddresses peer0.org3.digiblocks.com:11051 --tlsRootCertFiles $PEER0_ORG3_CA   \
         --peerAddresses peer0.org5.digiblocks.com:15051 --tlsRootCertFiles $PEER0_ORG5_CA   \
-        -c '{"function": "createModel","Args":["{\"ModelID\":\"model_autoencoder_attack_3\",\"NodeID\":\"value1\", \"ModelDescription\":\"Autoencoder for flooding attack detection\",\"MalciousPrecision\": 0.99, \"MalciousRecall\": 0.99, \"BenignPrecision\": 0.99, \"BenignRecall\": 0.99, \"Hash\":\"eyJleHAiOjE2MjcwMTc5NDUsInghgdfduYW1lIjoiVGVzdCIsIm9yZ05hbWUiOiJPcmcxIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjI2OTgxOTQ1fQ\", \"TrustComposition\": \"QoS\"}"]}'
-
-
-
+        -c '{"function": "createModel","Args":["{\"ModelID\":\"model_svm_attack_1\",\"NodeID\":\"value1\", \"ModelDescription\":\"Autoencoder for flooding attack detection\",\"MalciousPrecision\": 0.99, \"MalciousRecall\": 0.99, \"BenignPrecision\": 0.99, \"BenignRecall\": 0.99, \"Hash\":\"eyJleHAiOjE2MjcwMTc5NDUsInghgdfduYW1lIjoiVGVzdCIsIm9yZ05hbWUiOiJPcmcxIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjI2OTgxOTQ1fQ\", \"TrustComposition\": \"QoS\"}"]}'
 
 
     ## Init ledger
